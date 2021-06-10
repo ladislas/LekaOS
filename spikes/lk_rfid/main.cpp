3d05a078bcc9b1549699485a2f27f6987aefbccd
Benjamin Roussilhe <benjamin.roussilhe@gmail.com>
2021-06-10 15:39:07 +0200

    :construction: (spikes): Add spike to test CoreCR95HF and RFIDKit

--- spikes/lk_rfid/main.cpp
+++ spikes/lk_rfid/main.cpp
@@ -2,21 +2,33 @@
 // Copyright 2020 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
+#include <cstdint>
+
 #include "drivers/BufferedSerial.h"
 #include "rtos/ThisThread.h"
 #include "rtos/Thread.h"
 
+#include "CoreBufferedSerial.h"
+#include "CoreCR95HF.h"
 #include "HelloWorld.h"
-#include "LekaRFID.h"
 #include "LogKit.h"
+#include "RFIDKit.h"
 
 using namespace leka;
 using namespace std::chrono;
 
 auto main() -> int
 {
-	static auto serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
-	leka::logger::set_print_function([](const char *str, size_t size) { serial.write(str, size); });
+	static auto log_serial = mbed::BufferedSerial(USBTX, USBRX, 115200);
+	leka::logger::set_print_function([](const char *str, size_t size) { log_serial.write(str, size); });
+
+	static auto mbed_serial = mbed::BufferedSerial(RFID_UART_TX, RFID_UART_RX, 57600);
+	static auto rfid_serial = CoreBufferedSerial(mbed_serial);
+	static auto rfid_reader = CoreCR95HF(rfid_serial);
+	static auto core_rfid	= RFIDKit(rfid_reader);
+
+	core_rfid.init();
+	std::array<uint8_t, 16> tag_data {};
 
 	auto start = rtos::Kernel::Clock::now();
 
@@ -24,17 +36,18 @@ auto main() -> int
 
 	rtos::ThisThread::sleep_for(2s);
 
-	RFID leka_rfid;
-	rtos::Thread rfid_thread;
-	rfid_thread.start({&leka_rfid, &RFID::start});
-
 	HelloWorld hello;
 	hello.start();
 
 	while (true) {
 		auto t = rtos::Kernel::Clock::now() - start;
-		log_info("A message from your board %s --> \"%s\" at %i s\n", MBED_CONF_APP_TARGET_NAME, hello.world,
-				 int(t.count() / 1000));
+		core_rfid.getTagData(tag_data);
+
+		printf("TagData : ");
+		for (int i = 0; i < tag_data.size(); ++i) {
+			printf("%i ,", tag_data.data()[i]);
+		}
+		printf("\n");
 
 		rtos::ThisThread::sleep_for(1s);
 	}
