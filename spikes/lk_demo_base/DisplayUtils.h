185a248f83b493dd742675a563ba42efd51e58cc
Yann Locatelli <yann.locatelli2@gmail.com>
2021-11-23 08:01:00 +0100

    :recycle: (demo): TEST - LCD - Set out of class images table

--- spikes/lk_demo_base/DisplayUtils.h
+++ spikes/lk_demo_base/DisplayUtils.h
@@ -18,69 +18,78 @@
 
 namespace leka {
 
+constexpr std::array<const char *, 51> image_table = {
+	"leka-logo",
+	"shape-circle-plain_line",
+	"shape-square-plain_line",
+	"shape-triangle-plain_line",
+	"shape-star-plain_line",
+	"shape-circle-dotted_line",
+	"shape-square-dotted_line",
+	"shape-triangle-dotted_line",
+	"shape-star-dotted_line",
+	"number-0_zero",
+	"number-1_one",
+	"number-2_two",
+	"number-3_three",
+	"number-4_four",
+	"number-5_five",
+	"number-6_six",
+	"number-7_seven",
+	"number-8_eight",
+	"number-9_nine",
+	"number-10_ten",
+	"color-red",
+	"color-blue",
+	"color-green",
+	"color-yellow",
+	"color-black",
+	"color-white",
+	"activity-magic_objects",
+	"activity-dance_freeze",
+	"activity-hide_and_seek",
+	"activity-music_colored_board",
+	"activity-music_quest",
+	"activity-color_quest",
+	"activity-colors_and_sounds",
+	"activity-super_simon",
+	"remote-colored_arrows",
+	"remote-standard",
+	"reinforcer-1-green-spin",
+	"reinforcer-2-violet_green_blink-spin",
+	"reinforcer-3-fire-static",
+	"reinforcer-4-glitters-static",
+	"reinforcer-5-rainbow-static",
+	"emotion-happiness-child",
+	"emotion-sadness-child",
+	"emotion-anger-child",
+	"emotion-fear-child",
+	"emotion-disgust-child",
+	"robot-emotion-happy",
+	"robot-emotion-sad",
+	"robot-emotion-angry",
+	"robot-emotion-affraid",
+	"robot-emotion-disgusted",
+};
+
+constexpr std::array<const char *, 9> emotion_table = {
+	"robot-emotion-happy",		 "robot-emotion-angry",		"robot-emotion-affraid",
+	"robot-emotion-disgusted",	 "robot-emotion-sad_tears", "robot-emotion-sad",
+	"robot-emotion-tired_tears", "robot-emotion-tired",		"robot-emotion-neutral"};
+
+const std::array<std::string, 4> object_colored_family_table = {
+	"animal-fish",
+	"object-balloon",
+	"object-beach_bucket",
+	"plant-flower-5_petals",
+
+};
+
+constexpr std::array<const char *, 3> video_table = {"animation-idle", "animation-joy", "animation-perplexity"};
+
 class DisplayUtils
 {
   public:
-	const std::array<const char *, 51> image_table = {
-		"leka-logo",
-		"shape-circle-plain_line",
-		"shape-square-plain_line",
-		"shape-triangle-plain_line",
-		"shape-star-plain_line",
-		"shape-circle-dotted_line",
-		"shape-square-dotted_line",
-		"shape-triangle-dotted_line",
-		"shape-star-dotted_line",
-		"number-0_zero",
-		"number-1_one",
-		"number-2_two",
-		"number-3_three",
-		"number-4_four",
-		"number-5_five",
-		"number-6_six",
-		"number-7_seven",
-		"number-8_eight",
-		"number-9_nine",
-		"number-10_ten",
-		"color-red",
-		"color-blue",
-		"color-green",
-		"color-yellow",
-		"color-black",
-		"color-white",
-		"activity-magic_objects",
-		"activity-dance_freeze",
-		"activity-hide_and_seek",
-		"activity-music_colored_board",
-		"activity-music_quest",
-		"activity-color_quest",
-		"activity-colors_and_sounds",
-		"activity-super_simon",
-		"remote-colored_arrows",
-		"remote-standard",
-		"reinforcer-1-green-spin",
-		"reinforcer-2-violet_green_blink-spin",
-		"reinforcer-3-fire-static",
-		"reinforcer-4-glitters-static",
-		"reinforcer-5-rainbow-static",
-		"emotion-happiness-child",
-		"emotion-sadness-child",
-		"emotion-anger-child",
-		"emotion-fear-child",
-		"emotion-disgust-child",
-		"emotion-happiness-leka",
-		"emotion-sadness-leka",
-		"emotion-anger-leka",
-		"emotion-fear-leka",
-		"emotion-disgust-leka",
-	};
-
-	const std::array<const char *, 8> emotion_table = {
-		"robot-emotion-happy",	   "robot-emotion-angry", "robot-emotion-affraid",	   "robot-emotion-disgusted",
-		"robot-emotion-sad_tears", "robot-emotion-sad",	  "robot-emotion-tired_tears", "robot-emotion-neutral"};
-
-	const std::array<const char *, 3> video_table = {"animation-idle", "animation-joy", "animation-perplexity"};
-
 	explicit DisplayUtils(rtos::Thread &video_thread, rtos::EventFlags &event_flags, LKCoreSTM32Hal &hal,
 						  CoreSDRAM &sdram, VideoKit &display)
 		: _video_thread(video_thread), _event_flags(event_flags), _hal(hal), _coresdram(sdram), _display(display) {};
